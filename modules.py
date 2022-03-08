14dac7a116abc3cf5ed8de4266f53852b400b406
mic-rusti-bread <michele.rusticucci@gmail.com>
2022-03-09 00:03:08 +0100

    Penultimi ritocchi - inizio test
    Manca poco, bisogna scrivere gli unittest con la libreria appostita

--- modules.py
+++ modules.py
@@ -1,30 +1,24 @@
 from datetime import datetime
 import json
+# from nis import cat
 import pickle
 
 class Activity:
-    def __init__(self, titolo, id=0):
+    def __init__(self, title, id=0):
         self.id = id
-        self.titolo = titolo
+        self.title = self.setTitle(title)
         self.timestamp = datetime.timestamp(datetime.now()) 
         self.done = False
 
-    @property
-    def titolo(self):
-        return self._titolo
-    @titolo.setter
-    def titolo(self, value):
-        if len(value) < 5:
-            raise ValueError("Titolo troppo corto!")
-        self._titolo = value
+
+    def setTitle(self, value, validate = True):
+        if validate:
+            if len(value) < 5:
+                raise ValueError("Title too short. Must be longer than 5 characters")
+        self.title = value
         
-
     def getTitle(self):
-        return self.titolo
-
-    def setTitle(self, nuovoTitolo):
-        self.titolo = nuovoTitolo
-
+        return self.title
 
     def setToggleDone(self):
         self.done = not self.done
@@ -33,15 +27,16 @@ class Activity:
         return json.dumps(self.__dict__)
 
 
-#si potrebbe aggiungere un dizionaro che tiene conto di qual#
+#si potrebbe aggiungere un dizionaro che tiene conto di qual
 #id appartiene a quale indice nell'array per velocizzare la ricerca
+#visto che al momento la property id viene considerata un identificativio e non un indice array
 class ToDo:
     def __init__(self):
         self.list = []
         self.loadFromStorage()
 
-    def getList(self):
-        return self.list
+    def getList(self, orderBy, reverseParam=False):
+        return sorted(self.list, key=lambda activity: getattr(activity, orderBy), reverse=reverseParam)
 
     def loadFromStorage(self):
         file = open("localPersist.txt","rb") 
@@ -55,21 +50,28 @@ class ToDo:
             pickle.dump(self.list, file)
 
 
-    def add(self, titolo):
+    def add(self, title):
         id = 0 #lo metto qui perche voglio che sia un progressivo
         # aggiungo 1 al numero di index massimo contenuto visto che potrebbe non corrispondere alla lunghezza della list
         lengthList = len(self.list)
         if lengthList > 0:
             id = self.list[lengthList - 1].id + 1           
 
-        newActivity = Activity(titolo, id)
-        self.list.append(newActivity)
+        try:
+            newActivity = Activity(title, id)
+            self.list.append(newActivity)
+        except ValueError as err:
+            return 'Validation error:\n ' + str(err)
 
-    def edit(self, id, newTitle):
+    def editTitle(self, id, newTitle):
         obj = self.searchById(id)
         if not obj:
             return "not found"
-        self.list[obj.id].setTitle(newTitle)
+
+        try:
+            self.list[obj.id].setTitle(newTitle)
+        except ValueError as err:
+            return 'Validation error:\n ' + str(err)
 
     def remove(self, id):
         obj = self.searchById(id)
@@ -100,11 +102,3 @@ class ToDo:
                 return elem
         return False    
     
-
-
-
-##Appunti
-#Aggiungere validazione, non so se nella classe del ToDo o nel modello
-#Nel modello starebbe male perche ci sono solo le cose dell'oggetto ma potremmo metterci   
-#il salvataggio a db direttamente. In questo modo abbiamo una divisione effettiva
-#tra chi interagisce con i dati e chi li mette a db. 
--- todo_manager.py
+++ todo_manager.py
@@ -2,43 +2,46 @@ import argparse
 from modules import ToDo
 
 #init vars
-myList = ToDo()
+todo_list = ToDo()
 parser = argparse.ArgumentParser(prog='todo_manager')
+returnValue = '' 
 
 #set args
-parser.add_argument("-ls", '--list', action="store_true")
-parser.add_argument("-a", '--add', nargs=1, type=str)
-parser.add_argument("-e", '--edit', nargs=2)
-parser.add_argument("-d", '--delete', nargs=1, type=int)
-parser.add_argument("-t", '--toggle', nargs=1, type=int)
-parser.add_argument("-s", '--search', nargs=1, type=str)
-
-# parser.add_argument("title", help="echo the string you use here", type=str)
+parser.add_argument("-ls", '--list', action="store_true", help='Lists all values ordered by desc(timestamp)')
+parser.add_argument("-a", '--add', nargs=1, type=str, help="Adds a new activity. --> Pass ['newTitle'] as param. Must be longer than 5 chars")
+parser.add_argument("-e", '--edit', nargs=2, help = "Edits an existing activity. --> Pass ['id'] and ['new Title'] as params")
+parser.add_argument("-d", '--delete', nargs=1, type=int, help="Deletes an existing activity. --> Pass ['id'] as param")
+parser.add_argument("-t", '--toggle', nargs=1, type=int, help="Toggles ['done'] true/false of an existing activity. --> Pass ['id'] as param")
+parser.add_argument("-s", '--search', nargs=1, type=str, help="Searches and existing activity --> Pass ['id'] as param")
 args = parser.parse_args()
-returnValue = ''
-#case decision
+
+
 if args.add:
-    returnValue = myList.add(args.add[0])
+    returnValue = todo_list.add(args.add[0])
 
 elif args.edit:    
     try:    
         i = int(args.edit[0])
-        myList.edit(i, args.e[1])
-    except:
-        print('si errore')
+        todo_list.editTitle(i, args.edit[1])
+    except ValueError:
+        returnValue = 'Value of argument(/s) unexpected, refer to [-h | --help] for more information '
 
 elif args.delete:
-    returnValue = myList.remove(args.delete[0])    
+    returnValue = todo_list.remove(args.delete[0])    
 
 elif args.toggle:
-    returnValue = myList.toggleDone(args.toggle[0])        
+    returnValue = todo_list.toggleDone(args.toggle[0])        
 
 elif args.search:
-    returnValue = myList.searchByTitle(args.search[0])
+    returnValue = todo_list.searchByTitle(args.search[0])
 
 elif args.list:
-    returnValue = myList.getList()
+    returnValue = todo_list.getList('id', True)
 
 if returnValue : print(returnValue)
+
 #salvo
-myList.saveToStorage()
+todo_list.saveToStorage()
+
+
+
