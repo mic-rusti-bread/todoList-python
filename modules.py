efef895ab10c7a7c5ab31b80fa72f8e8758601a2
mic-rusti-bread <michele.rusticucci@gmail.com>
2022-03-08 01:58:25 +0100

    finita la prima bozza. da concludere
    Da aggiungere ancora il livello di validazione e da rivedere le interazioni di persistenza.
    Guarda note a fine pagina

--- modules.py
+++ modules.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 import json
-
+import pickle
 
 class Activity:
-    def __init__(self, titolo):
+    def __init__(self, titolo, id=''):
+        self.id = id
         self.titolo = titolo
         self.timestamp = datetime.timestamp(datetime.now()) 
         self.done = False
@@ -11,10 +12,10 @@ class Activity:
     def getTitle(self):
         return self.titolo
 
-    def edit(self, nuovoTitolo):
+    def setTitle(self, nuovoTitolo):
         self.titolo = nuovoTitolo
 
-    def toggleDone(self):
+    def setToggleDone(self):
         self.done = not self.done
 
     def __repr__(self): 
@@ -25,38 +26,82 @@ class Activity:
 class ToDo:
     def __init__(self):
         self.list = []
-    
-#Quando carica, lui mette le righe come testo dentro il la lista, invece dobbiamo trasformarle in oggetto
+        self.loadFromStorage()
+
+    def getList(self):
+        return self.list
+
     def loadFromStorage(self):
-        file = open("localPersist.txt","r") 
-        self.list = file.readlines()
-        file.close()
-    
-    def saveToStorage(self):
-        file = open("localPersist.txt","w+")
-        for item in self.list:
-            file.write("%s\n" % item)  
-        file.close
+        file = open("localPersist.txt","rb") 
+        try:
+            self.list = pickle.load(file)
+        except:
+            self.list = []
+  
+    def saveToStorage(self):       
+        with open('localPersist.txt', 'wb') as file:
+            pickle.dump(self.list, file)
+
 
     def add(self, titolo):
-        newActivity = Activity(titolo)
+        if len(titolo) < 5:
+            return 'Titolo troppo corto'
+
+        #lo metto qui perche voglio che sia un progressivo
+        id = 0
+
+        # aggiungo 1 al numero di index massimo contenuto visto che potrebbe non corrispondere alla lunghezza della list
+        lengthList = len(self.list)
+        if lengthList > 0:
+            id = self.list[lengthList - 1].id + 1           
+
+        newActivity = Activity(titolo, id)
         self.list.append(newActivity)
 
-    def remove(self, index):
-        return self.list.pop(index)
-    
-    def edit(self, index, newTitle):
-        self.list[index].edit(newTitle)
+    def remove(self, id):
+        obj = self.searchById(id)
+        if obj:
+            return "not found"
+        
+        self.list.remove(obj)    
 
-    def toggleDone(self, index):
-        self.list[index].toggleDone()
+    def edit(self, id, newTitle):
+        obj = self.searchById(id)
+        if not obj :
+            return "not found"
+        
+        self.list[obj.id].setTitle(newTitle)
 
-    def search(self, stringToSearch):
+    def toggleDone(self, id):
+        obj = self.searchById(id)
+        if not obj:
+            return "not found"
+
+        self.list[obj.id].setToggleDone()
+
+    def searchByTitle(self, titleToSearch):
         returnList = []
-        stringToSearch = stringToSearch.lower()
+        titleToSearch = titleToSearch.lower()
 
         for elem in self.list:
-            if stringToSearch in elem.getTitle().lower():
+            if titleToSearch in elem.getTitle().lower():
                 returnList.append(elem)
 
         return returnList
+
+    def searchById(self, id):
+        for elem in self.list:
+            if id == elem.id:
+                return elem
+        return False    
+    
+    def validate(self, id, titolo, ):
+        pass
+
+
+
+##Appunti
+#Aggiungere validazione, non so se nella classe del ToDo o nel modello
+#Nel modello starebbe male perche ci sono solo le cose dell'oggetto ma potremmo metterci   
+#il salvataggio a db direttamente. In questo modo abbiamo una divisione effettiva
+#tra chi interagisce con i dati e chi li mette a db. 
--- todo_manager.py
+++ todo_manager.py
@@ -1,20 +1,44 @@
 import argparse
 from modules import ToDo
-# from modules import ListManager
 
-# parser = argparse.ArgumentParser(description='Process some integers.')
-# parser.add_argument('action', help='add', nargs=2, choices=['a', 'e'])
-# args = parser.parse_args()
-# print(args)
-
+#init vars
 myList = ToDo()
-myList.loadFromStorage()
-myList.add('myTodo01')
-myList.add('myTodo02')
-myList.add('myTodo03')
-myList.add('myTodo04')
-myList.edit(0, 'nuovo titolo')
-myList.toggleDone(0)
-print(myList.list)
+parser = argparse.ArgumentParser(prog='todo_manager')
 
-# myList.saveToStorage()
+#set args
+parser.add_argument("-ls", '--list', action="store_true")
+parser.add_argument("-a", '--add', nargs=1, type=str)
+parser.add_argument("-e", '--edit', nargs=2)
+parser.add_argument("-d", '--delete', nargs=1, type=int)
+parser.add_argument("-t", '--toggle', nargs=1, type=int)
+parser.add_argument("-s", '--search', nargs=1, type=str)
+
+# parser.add_argument("title", help="echo the string you use here", type=str)
+args = parser.parse_args()
+returnValue = ''
+#case decision
+if args.add:
+    returnValue = myList.add(args.add[0])
+
+elif args.edit:    
+    try:    
+        i = int(args.edit[0])
+        myList.edit(i, args.e[1])
+    except:
+        print('si errore')
+
+elif args.delete:
+    returnValue = myList.remove(args.delete[0])    
+
+elif args.toggle:
+    returnValue = myList.toggleDone(args.toggle[0])        
+
+elif args.search:
+    returnValue = myList.searchByTitle(args.search[0])
+
+elif args.list:
+    returnValue = myList.getList()
+
+if returnValue : print(returnValue)
+#salvo
+myList.saveToStorage()
